85d7aed3740bb3efd14a13e00fa142331623f2c5
Noah Albers <albers-noah@pm.me>
2023-03-20 18:11:59 +0100

    - Added image-counter to statistics

--- src/ui/components/UiStatistics.py
+++ src/ui/components/UiStatistics.py
@@ -4,6 +4,7 @@ from src.data.Project import Project
 from src.ui import Signals, UserRessources
 from src.utils import EventDispatcher
 
+
 @Gtk.Template.from_file("rsc/glade/Statistics.glade")
 class Statistics(Gtk.Box):
     __gtype_name__ = "baseStatistics"
@@ -11,13 +12,16 @@ class Statistics(Gtk.Box):
     fld_amt_projects: Gtk.Label = Gtk.Template.Child("amt_projects")
     fld_amt_projects_with_imgs: Gtk.Label = Gtk.Template.Child("amt_projects_wimg")
     fld_amt_projects_without_imgs: Gtk.Label = Gtk.Template.Child("amt_projects_nimg")
+    fld_amt_images: Gtk.Label = Gtk.Template.Child("amt_images")
 
     def __set_loaded_project(self, amt: str | None):
         self.fld_amt_projects.set_text("" if amt is None else amt)
 
-    def __set_loaded_images(self, proj_with: str | None, proj_without: str | None):
+    def __set_loaded_images(self, proj_with: str | None, proj_without: str | None, total: str | None):
         self.fld_amt_projects_with_imgs.set_text("" if proj_with is None else proj_with)
         self.fld_amt_projects_without_imgs.set_text("" if proj_without is None else proj_without)
+
+        self.fld_amt_images.set_text("" if total is None else total)
 
     def __update_internal_ui(self, _):
         # Checks if projects are loaded
@@ -29,15 +33,17 @@ class Statistics(Gtk.Box):
 
         # Checks if images are loaded
         if UserRessources.project_images is None:
-            self.__set_loaded_images(None, None)
+            self.__set_loaded_images(None, None, None)
 
         if UserRessources.project_images is not None and UserRessources.projects is not None:
             # Calculates the amount of projects with and without an image
-            amt_with = len(list(filter(lambda proj: proj.get_raw_stand_number() in UserRessources.project_images, UserRessources.projects)))
+            amt_with = len(list(filter(lambda proj: proj.get_raw_stand_number() in UserRessources.project_images,
+                                       UserRessources.projects)))
 
-            amt_without = len(list(filter(lambda proj: proj.get_raw_stand_number() not in UserRessources.project_images, UserRessources.projects)))
+            amt_without = len(list(filter(lambda proj: proj.get_raw_stand_number() not in UserRessources.project_images,
+                                          UserRessources.projects)))
 
-            self.__set_loaded_images(str(amt_with), str(amt_without))
+            self.__set_loaded_images(str(amt_with), str(amt_without), str(len(UserRessources.project_images)))
 
     def __init__(self):
         super().__init__()
